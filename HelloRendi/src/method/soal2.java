c95236d8ecc082ef7a245729ef686b920874dbe1
rendiputra <rendiputrapradana@gmail.com>
2020-12-16 23:53:04 +0700

    revisi soal2 di folder method

--- HelloRendi/src/method/soal2.java
+++ HelloRendi/src/method/soal2.java
@@ -5,30 +5,19 @@ package method;
     Kelas   : SE04C
  */
 import java.util.Scanner; // import library Scanner
-
 public class soal2 { // membuat class "soal2"
     public static void main(String[] args) { // function "main" (utama)
         Scanner scan = new Scanner(System.in); //  membuat scanner baru dengan nama scan
         System.out.print("Masukan jari-jari : ");
         double r = scan.nextDouble(); // membuat variabel r lalu mengambil inputan user dengan bantuan library scanner
-        double
-            luas     = luas_lingakaran(r), // menginisialisasi variabel luas dengan memanggil function luas_persegi() dengan parameternya adalah variabel r
-            keliling = keliling_lingakaran(r) // menginisialisasi variabel keliling dengan memanggil function keliling_persegi() dengan parameternya adalah variabel r
-        ;
-
-        System.out.println("Keliling Lingkaran : " + keliling); // mencetak output Keliling Lingkaran dengan variabel "Keliling"
-        System.out.println("Luas Lingkaran      : " + luas); // mencetak output Luas Lingkaran dengan variabel "Keliling"
+        lingkaran(r); // memanggil method lingkaran()
     }
 
     // Method Procedure
-
-    // Method untuk menghitung keliling lingkaran
-    static double keliling_lingakaran(double r){ // function "keliling_lingakaran()" dengan parameter variabel "r"
-        return 2 * 3.14159265358979 * r; // akan mengembalikan nilai double dari hasil perhitungan rumus
-    }
-
-    // Method untuk menghitung Luas lingkaran
-    static double luas_lingakaran(double r){ // function "luas_lingakaran()" dengan parameter variabel "r"
-        return 3.14159265358979 * r * r; // akan mengembalikan nilai double dari hasil perhitungan rumus
+    static void lingkaran(double r){
+        double luas = 3.14159265358979 * r * r; // rumus luas = phi x r^2
+        double keliling = 2 * 3.14159265358979 * r; // rumus keliling = phi x r x 2 atau phi * d
+        System.out.println("Keliling Lingkaran : " + keliling); // mencetak output Keliling Lingkaran dengan variabel "Keliling"
+        System.out.println("Luas Lingkaran      : " + luas); // mencetak output Luas Lingkaran dengan variabel "Keliling"
     }
 }
